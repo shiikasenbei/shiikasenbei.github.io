e5fdad73f39b76a41102be26f74b3c9f094bf6db
shika <shiikasenbei@gmail.com>
2026-09-11 00:20:28 +1200

    remove discord

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,6 @@ export const SOCIAL_LINKS: SocialLink[] = [
   { name: "twitter", username: "@shiikasenbei", url: "https://x.com/shiikasenbei" },
   { name: "instagram", username: "@shiikasenbei", url: "https://instagram.com/shiikasenbei" },
   { name: "twitch", username: "@shiikasenbei", url: "https://twitch.tv/shiikasenbei" },
-  { name: "discord server", username: "shikacord", url: "https://discord.com/invite/qEXM5PzTqB" },
   { name: "pixiv", username: "shika", url: "https://www.pixiv.net/en/users/128340030" },
 ]
 export const WORK_ITEMS: WorkItem[] = [
