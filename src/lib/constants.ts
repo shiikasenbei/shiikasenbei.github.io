26844801999f0f9134a35c5f57486570836c60a7
shika <shiikasenbei@gmail.com>
2026-09-21 15:16:42 +1200

    update worklog again

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -18,7 +18,8 @@ export const WORK_ITEMS: WorkItem[] = [
     inProgress: true,
     confirmed: true,
   },
-  { name: "oc design (maybe?)", inProgress: false, confirmed: true },
+  { name: "shiori novella (again!)", inProgress: true, confirmed: true },
+  { name: "oc design (maybe?)", inProgress: true, confirmed: true },
   { name: "cecilia immergreen", inProgress: false, confirmed: true },
   { name: "raora panthera", inProgress: false, confirmed: false, candidate: true },
   {
