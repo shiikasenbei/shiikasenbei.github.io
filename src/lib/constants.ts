ac7a07a472033fd8f3969cdf3e97a5ffdb0c9264
shika <shiikasenbei@gmail.com>
2026-09-22 21:07:31 +1200

    Update constants in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -20,15 +20,14 @@ export const WORK_ITEMS: WorkItem[] = [
   },
   { name: "shiori novella (again!)", inProgress: true, confirmed: true },
   { name: "oc design", inProgress: true, confirmed: true },
-  { name: "gigi murin", inProgress: false, confirmed: true, extraneousText: "sep 21" },
 
   { name: "hyakuto kyoko", inProgress: false, confirmed: false, candidate: true },
-  { name: "raora panthera", inProgress: false, confirmed: false, candidate: false },
+  { name: "cecilia immergreen", inProgress: false, confirmed: false, candidate: true },
   {
     name: "more hololive related stuff",
     inProgress: false,
     confirmed: false,
-    ongoing: true,
+    extraneousText: "ongoing"
   },
 ]
 
